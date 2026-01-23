e65ecddd1b13baa10354bee84fc280240a15cea3
Eid Aden <e-aden@hotmail.de>
2026-01-23 17:11:44 +0100

    Mobile Menu  added

--- src/app/features/home/hero/hero.ts
+++ src/app/features/home/hero/hero.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SocialIcons } from '../../../shared/ui/social-icons/social-icons';
 import { ScrollIndicator } from '../../../shared/ui/scroll-indicator/scroll-indicator';
+import { MobileMenu } from '../../../shared/ui/mobile-menu/mobile-menu';
 
 @Component({
   selector: 'app-hero',
-  imports: [SocialIcons, ScrollIndicator],
+  imports: [SocialIcons, ScrollIndicator, MobileMenu],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
